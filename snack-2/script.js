563d771eac273805db82a744cf38c2d31ca7ea6f
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-21 14:11:07 +0100

    2 snack

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,13 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
+
+people.forEach((names) => {
+  console.log(`
+    ${names.name}`);
+});
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
